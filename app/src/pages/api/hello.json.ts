a37a958e52aea28ea758e1ceda3d8c86fe7b9ab4
kaerunrun <take.t.public@gmail.com>
2023-03-31 17:31:41 +0000

    try console.log

--- app/src/pages/api/hello.json.ts
+++ app/src/pages/api/hello.json.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async ({ params, request }) => {
+  // console.log("params", params);
+  // console.log("request", request);
   return {
     body: JSON.stringify({
       name: "astro",
